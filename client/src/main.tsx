009b453e1f283559e3535dc435ccc1002731d87a
jymeng18 <aizeuyn@gmail.com>
2026-07-23 20:28:54 -0700

    part of the previous commit, forgot to push

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import './index.css'
-import App from './App'
 import AuthPage from './pages/AuthPage'
 import LandingPage from './pages/LandingPage'
 
@@ -10,7 +9,6 @@ createRoot(document.getElementById('root')!).render(
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<AuthPage />} />
-      <Route path="/app" element={<App />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   </BrowserRouter>,
