a67bd6bc57710825298a3d4eae91495023570797
jymeng18 <aizeuyn@gmail.com>
2026-08-22 19:16:15 -0700

    Apply migration, fix err in docs/

--- docs/Backend/DatabaseMigrations.md
+++ docs/Backend/DatabaseMigrations.md
@@ -4,7 +4,7 @@
 
 ```bash
 cd server/Inkboard.Infra/
-dotnet ef migrations add <MigrationName> -s ../Inkboard.API --environment Production
+ASPNETCORE_ENVIRONMENT=Production dotnet ef migrations add <MigrationName> -s ../Inkboard.API
 ```
 
 `-s` points EF to the startup project (Inkboard.API) so it can resolve the connection string and compile.
@@ -13,7 +13,10 @@ dotnet ef migrations add <MigrationName> -s ../Inkboard.API --environment Produc
 
 ```bash
 cd server/Inkboard.Infra/
-dotnet ef database update -s ../Inkboard.API
+
+ASPNETCORE_ENVIRONMENT=Production dotnet ef database drop -s ../Inkboard.API
+
+ASPNETCORE_ENVIRONMENT=Production dotnet ef database update -s ../Inkboard.API
 ```
 
 ## Connection string
---
+++ server/Inkboard.Infra/Migrations/20260823015613_InitialCreation.cs
@@ -0,0 +1,388 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Inkboard.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserName = table.Column<string>(type: "text", nullable: true),
+                    Email = table.Column<string>(type: "text", nullable: true),
+                    PasswordHash = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Block_List",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    BlockedUserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Block_List", x => new { x.BlockedUserId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_Block_List_Users_BlockedUserId",
+                        column: x => x.BlockedUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Block_List_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Canvases",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    OwnerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    SnapshotURL = table.Column<string>(type: "text", nullable: true),
+                    SnapshotTakenAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    LastModifiedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Canvases", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Canvases_Users_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Friend_Requests",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    RequesterId = table.Column<Guid>(type: "uuid", nullable: false),
+                    RequesteeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Friend_Requests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Friend_Requests_Users_RequesteeId",
+                        column: x => x.RequesteeId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Friend_Requests_Users_RequesterId",
+                        column: x => x.RequesterId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Friendships",
+                columns: table => new
+                {
+                    UserId1 = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId2 = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Friendships", x => new { x.UserId1, x.UserId2 });
+                    table.CheckConstraint("CK_Friendships_UserOrder", "\"UserId1\" < \"UserId2\"");
+                    table.ForeignKey(
+                        name: "FK_Friendships_Users_UserId1",
+                        column: x => x.UserId1,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Friendships_Users_UserId2",
+                        column: x => x.UserId2,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RefreshTokens",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    TokenHash = table.Column<string>(type: "text", nullable: true),
+                    ExpiresAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    IsRevoked = table.Column<bool>(type: "boolean", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RefreshTokens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RefreshTokens_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Canvas_Operations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    OperationData = table.Column<string>(type: "jsonb", nullable: false),
+                    Timestamp = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    CanvasId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Canvas_Operations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Canvas_Operations_Canvases_CanvasId",
+                        column: x => x.CanvasId,
+                        principalTable: "Canvases",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Canvas_Operations_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Parties",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    LeaderId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CanvasId = table.Column<Guid>(type: "uuid", nullable: true),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parties", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Parties_Canvases_CanvasId",
+                        column: x => x.CanvasId,
+                        principalTable: "Canvases",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_Parties_Users_LeaderId",
+                        column: x => x.LeaderId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Party_Invites",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    PartyId = table.Column<Guid>(type: "uuid", nullable: false),
+                    InvitedByUserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    InvitedUserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    InviteStatus = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    ExpiresAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Party_Invites", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Party_Invites_Parties_PartyId",
+                        column: x => x.PartyId,
+                        principalTable: "Parties",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Party_Invites_Users_InvitedByUserId",
+                        column: x => x.InvitedByUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Party_Invites_Users_InvitedUserId",
+                        column: x => x.InvitedUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Party_Members",
+                columns: table => new
+                {
+                    PartyId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Role = table.Column<int>(type: "integer", nullable: false),
+                    JoinedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Party_Members", x => new { x.PartyId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_Party_Members_Parties_PartyId",
+                        column: x => x.PartyId,
+                        principalTable: "Parties",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Party_Members_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Block_List_UserId",
+                table: "Block_List",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Canvas_Operations_CanvasId_Timestamp",
+                table: "Canvas_Operations",
+                columns: new[] { "CanvasId", "Timestamp" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Canvas_Operations_UserId",
+                table: "Canvas_Operations",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Canvases_OwnerId",
+                table: "Canvases",
+                column: "OwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Friend_Requests_RequesteeId",
+                table: "Friend_Requests",
+                column: "RequesteeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Friend_Requests_RequesterId_RequesteeId",
+                table: "Friend_Requests",
+                columns: new[] { "RequesterId", "RequesteeId" },
+                unique: true,
+                filter: "\"Status\" = 0");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Friendships_UserId2",
+                table: "Friendships",
+                column: "UserId2");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Parties_CanvasId",
+                table: "Parties",
+                column: "CanvasId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Parties_LeaderId",
+                table: "Parties",
+                column: "LeaderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Party_Invites_InvitedByUserId",
+                table: "Party_Invites",
+                column: "InvitedByUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Party_Invites_InvitedUserId",
+                table: "Party_Invites",
+                column: "InvitedUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Party_Invites_PartyId_InvitedUserId",
+                table: "Party_Invites",
+                columns: new[] { "PartyId", "InvitedUserId" },
+                unique: true,
+                filter: "\"InviteStatus\" = 0");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Party_Members_UserId",
+                table: "Party_Members",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RefreshTokens_TokenHash",
+                table: "RefreshTokens",
+                column: "TokenHash",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RefreshTokens_UserId",
+                table: "RefreshTokens",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Block_List");
+
+            migrationBuilder.DropTable(
+                name: "Canvas_Operations");
+
+            migrationBuilder.DropTable(
+                name: "Friend_Requests");
+
+            migrationBuilder.DropTable(
+                name: "Friendships");
+
+            migrationBuilder.DropTable(
+                name: "Party_Invites");
+
+            migrationBuilder.DropTable(
+                name: "Party_Members");
+
+            migrationBuilder.DropTable(
+                name: "RefreshTokens");
+
+            migrationBuilder.DropTable(
+                name: "Parties");
+
+            migrationBuilder.DropTable(
+                name: "Canvases");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
